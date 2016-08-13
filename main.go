f17438cda021e6676d4dfdef304874115233e0f7
Marnes Prinsloo <marnes.prinsloo@gmail.com>
2016-08-13 16:08:12 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/register", index).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	log.Println("Responsing to /hello request")
+	log.Println(r.UserAgent())
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Hello:", name)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	log.Println("Responsing to /hello request")
+	log.Println(r.UserAgent())
+
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Hello:", name)
+}
